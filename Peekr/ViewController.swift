473675db8dbcad23c08c7f15a1c0321d2f176301
Mounir Ybanez <rinuom91@gmail.com>
2019-08-26 15:07:25 +0800

    Initial commit.

---
+++ Peekr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Peekr
+//
+//  Created by Mounir Ybanez on 8/26/19.
+//  Copyright © 2019 Nir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '10.0'
+use_frameworks!
+inhibit_all_warnings!
+
+def firebase_pods
+    pod 'Firebase/Core'
+    pod 'Firebase/Auth'
+    pod 'Firebase/Firestore'
+end
+
+target 'Peekr' do
+    firebase_pods
+end
+
