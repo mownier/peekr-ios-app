0f15f35711c772abd819a86b5e147d05df8d3ba3
Mounir Ybanez <rinuom91@gmail.com>
2019-09-06 14:27:42 +0800

    Added home component.

---
+++ Peekr/Components/Home/HomePagingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomePagingViewController.swift
+//  Peekr
+//
+//  Created by Mounir Ybanez on 9/6/19.
+//  Copyright © 2019 Nir. All rights reserved.
+//
+
+import UIKit
+
+class HomePagingViewController: UIPageViewController {
+
+}
