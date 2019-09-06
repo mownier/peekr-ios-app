098b8add820787f0c64fdd4882711911dbe57552
Mounir Ybanez <rinuom91@gmail.com>
2019-09-06 14:46:45 +0800

    Added home strings.

---
+++ Peekr/Components/Home/HomeStrings.swift
@@ -0,0 +1,12 @@
+//
+//  HomeStrings.swift
+//  Peekr
+//
+//  Created by Mounir Ybanez on 9/6/19.
+//  Copyright © 2019 Nir. All rights reserved.
+//
+
+enum HomeStrings {
+    
+    static let showMyProfileScreenNotificationRawName = "HomeStrings.showMyProfileScreen"
+}
