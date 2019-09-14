f2cbdcb84e38e95d8edb1a35f34bce0c0d265aa4
Mounir Ybanez <rinuom91@gmail.com>
2019-09-14 10:46:07 +0800

    Added composer strings.

--- Peekr/Components/Composer/ComposerStrings.swift
+++ Peekr/Components/Composer/ComposerStrings.swift
@@ -8,9 +8,13 @@
 
 enum ComposerStrings {
     
+    static let cancelComposingNotificationRawName = "PostComposerViewController.cancelComposingNotification"
+    static let shareNotificationRawName = "PostComposerViewController.shareNotification"
     static let doneNotificationRawName = "GalleryViewController.doneNotification"
     static let dismissNotificationRawName = "GalleryViewController.dismissNotification"
     
     static let next = "Next".localized()
     static let gallery = "Gallery".localized()
+    static let share = "Share".localized()
+    static let shareTo = "Share To".localized()
 }
