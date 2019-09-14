af4ef5f4af09dd65c51d0356de9d566b12c13684
Mounir Ybanez <rinuom91@gmail.com>
2019-09-14 10:46:25 +0800

    Added creation of post composer screen.

--- Peekr/Components/Composer/ComposerBuilder.swift
+++ Peekr/Components/Composer/ComposerBuilder.swift
@@ -6,7 +6,14 @@
 //  Copyright © 2019 Nir. All rights reserved.
 //
 
+import Photos
+
 func createGalleryViewController() -> GalleryViewController {
     return viewControllerFromStoryboardWith(name: "Composer")
 }
 
+func createPostComposerViewController(asset: PHAsset) -> PostComposerViewController {
+    let screen: PostComposerViewController = viewControllerFromStoryboardWith(name: "Composer")
+    screen.asset = asset
+    return screen
+}
