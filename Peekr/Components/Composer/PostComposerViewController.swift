0eef3ba4a003ff649b0f20913ce01dec7614fb95
Mounir Ybanez <rinuom91@gmail.com>
2019-09-14 10:47:24 +0800

    Set up controls and notification.

--- Peekr/Components/Composer/PostComposerViewController.swift
+++ Peekr/Components/Composer/PostComposerViewController.swift
@@ -7,11 +7,37 @@
 //
 
 import UIKit
+import Photos
 
 class PostComposerViewController: UIViewController {
 
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var messageTextView: UITextView!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var titleButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
+    
+    var asset: PHAsset!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        messageTextView.layer.cornerRadius = 3.0
+        
+        shareButton.setTitle(ComposerStrings.share, for: .normal)
+        titleButton.setTitle(ComposerStrings.shareTo, for: .normal)
+        
+        thumbnailImageView.setImageFor(asset: asset)
     }
+    
+    @IBAction func onTapCloseButton() {
+        broadcastWith(name: PostComposerViewController.cancelComposingNotification, info: self)
+    }
+    
+    @IBAction func onTapShareButton() {
+        
+    }
+    
+    static let shareNotification = Notification.Name(rawValue: ComposerStrings.shareNotificationRawName)
+    static let cancelComposingNotification = Notification.Name(rawValue: ComposerStrings.cancelComposingNotificationRawName)
 }
