211824c5b85f90f279435aecff2ba907f327cd1f
Mounir Ybanez <rinuom91@gmail.com>
2020-02-08 12:10:31 +0800

    Added reload and updating of table view.

--- Peekr/Components/Search/UsersSearchResultScreen.swift
+++ Peekr/Components/Search/UsersSearchResultScreen.swift
@@ -12,25 +12,48 @@ class UsersSearchResultScreen: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var data: [Int] = (0..<9).map({ $0 })
+    var displayItems: [UserTableCell.DisplayItem] = []
     
     override func loadView() {
         super.loadView()
         
         tableView.register(UINib(nibName: "UserTableCell", bundle: nil), forCellReuseIdentifier: "UserTableCell")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        reload()
+    }
+    
+    @discardableResult
+    func displayItems(_ items: [UserTableCell.DisplayItem]) -> UsersSearchResultScreen {
+        displayItems = items
+        return self
+    }
+    
+    @discardableResult
+    func reload() -> UsersSearchResultScreen {
+        tableView?.reloadData()
+        tableView?.backgroundView = displayItems.isEmpty ? {
+            let label = UILabel()
+            label.textAlignment = .center
+            label.text = "No search result"
+            return label
+        }() : nil
+        return self
+    }
 }
 
 extension UsersSearchResultScreen: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return displayItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UserTableCell") as! UserTableCell
-        cell.textLabel?.text = "User \(data[indexPath.row])"
-        return cell
+        return cell.update(withItem: displayItems[indexPath.row])
     }
 }
 
